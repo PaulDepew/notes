15abb9c111930e8cacbcd3ebc5e27e4a0583300c
Paul Depew <pauldepew@comcast.net>
2020-06-08 19:03:11 -0700

    started and added --add

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 'use strict';
 
+/**
+ * This is a function.
+ *
+ * @param options - Entered from the Command line Arguments
+ * @return options - An Input Object
+ */
+
 // Node Module libraries
-const Input = require('./lib/add');
-const fetch = require('./lib/fetch');
+const Input = require('./lib/input');
+const Note = require('./lib/notes');
+
+
 
 
 const options = new Input();
+// console.log(options);
 
+
+function fetch(options) {
+  if (options.action === false) {
+    console.log('There was an error with your method! Please try again');
+  } else {
+    console.log(`Adding Note:  ${options.payload}`);
+  }
+}
 fetch(options);
+
+let note = new Note(options);
+
+console.log(note);
