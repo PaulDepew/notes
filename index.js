6e7178d374650929dfab6c9c6c66677678ed2e32
PaulDepew <Paul@lostamongpines.com>
2020-06-11 13:09:54 -0700

    fixed DB connection and CRUD functions

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 'use strict';
 
 // node modules
-const minimist = require('minimist');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Input = require('./lib/input');
 const Notes = require('./lib/notes');
 
-// new instance of constructor function from input.js file
+
+mongoose.connect(process.env.MONGODB_ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const userInput = new Input;
-console.log(userInput);
 
-if (userInput.validate(userInput) === true){
+const db = mongoose.connection;
+db.on('error', () => {
+  console.log('Database not connecting!');
+});
+db.on('open', () => {
+  console.log('connected to Atlas');
+  
+  if (userInput.validate(userInput) === true){
+  
+    // passes user input via above constructor to notes library
+    const noteAction = new Notes(userInput);
+    noteAction.execute();
+  
+    // console.log(noteAction);
+  } else { console.log('ERROR! Please use a proper command!');}
+});
 
-  // passes user input via above constructor to notes library
-  const addNote = new Notes(userInput);
-  addNote.execute(userInput);
-  // addNote.add(userInput);
 
-  console.log(addNote);
-} else { console.log('ERROR! Please insert a proper command!');}
+
 
 module.exports = userInput;
